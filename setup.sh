28534f45780bd5e02aebbf1c389669105269d196
saimanojk1 <saimanojk1@hotmail.com>
2025-09-30 20:48:31 +0200

    Update with tools being installed

--- setup.sh
+++ setup.sh
@@ -3,7 +3,14 @@ set -x
 brew install nvim
 brew install tmux
 brew install fzf
+brew install go
 git clone https://github.com/saimanojk1/tmuxifier.git ~/.tmuxifier
 echo ./zshrc >> ~/.zshrc
 git clone https://github.com/saimanojk1/nvim.git ~/.config/nvim
 echo ./.tmux.conf >> ~/.tmux.conf 
+xcode-select --install
+sudo /usr/sbin/DevToolsSecurity -enable
+sudo dscl . append /Groups/_developer GroupMembership $(whoami)
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/vektra/mockery/v3@v3.5.5
+go install github.com/nametake/golangci-lint-langserver@latest
